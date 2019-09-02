fe1ba12c8f61f7ccf21aad593135fb15b5469d0d
ChInMaYrOcKs <chinmaybhat1@gmail.com>
2019-09-02 09:00:20 +0530

    Props and Children props

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Let us get ReactV</h1>
-        <Person/>
+        <h1>Let's get ReactV</h1>
+        <Person name='Chinmay' age='27'>Hobbies: Badminton, Football, Carrom</Person>
+        <Person name='Chithra' age='30'/>
+        <Person name='Laxmi' age='55'/>
+        <Person name='Parameshwara' age='64'/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const person = () => {
+const person = (props) => {
     return(
-        <div>I am a Person Component</div>
+        <div>
+            <p>I am {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
+        </div>
     );
 }
 
