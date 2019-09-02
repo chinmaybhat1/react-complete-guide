6f7ca957761917e878184c3daf42e3f568dd6f1c
ChInMaYrOcKs <chinmaybhat1@gmail.com>
2019-09-02 09:13:12 +0530

    State in react

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name:"Chinmay", age:27},
+      {name:"Chithra", age:30},
+      {name:"Laxmi", age:55},
+      {name:"Parameshwara", age:64}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Let's get ReactV</h1>
-        <Person name='Chinmay' age='27'>Hobbies: Badminton, Football, Carrom</Person>
-        <Person name='Chithra' age='30'/>
-        <Person name='Laxmi' age='55'/>
-        <Person name='Parameshwara' age='64'/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobbies: Badminton, Football, Carrom</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
       </div>
     );
   }
