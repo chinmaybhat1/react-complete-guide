650d5d6d8e1818d8b99c732611ba8ce5d26324c1
ChInMaYrOcKs <chinmaybhat1@gmail.com>
2019-09-07 13:31:43 +0530

    Styling in React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserInput from './User/UserInput';
-import UserOutput from './User/UserOutput';
+import Person from './Person/Person';
 
 class App extends Component {
+  
+  state = {
+    persons: [
+      {name:"Chinmay", age:27},
+      {name:"Chithra", age:30},
+      {name:"Laxmi", age:55},
+      {name:"Parameshwara", age:64}
+    ]
+  }
 
-  state ={
-    name: "Batman"
+  switchStateHandler = (name) => {
+    this.setState({
+      persons: [
+        {name:name, age:26},
+        {name:"Chithra Bhat", age:30},
+        {name:"Laxmi P Bhat", age:55},
+        {name:"U Parameshwara Bhat", age:64}
+      ]
+    })
   }
 
   nameChangeHandler = (event) => {
     this.setState({
-      name: event.target.value
+      persons: [
+        {name:"Chinmay Bhat", age:26},
+        {name:event.target.value, age:30},
+        {name:"Laxmi P Bhat", age:55},
+        {name:"U Parameshwara Bhat", age:64}
+      ]
     })
   }
 
   render() {
     return (
       <div className="App">
-        <UserInput changed={this.nameChangeHandler} name={this.state.name}/>
-        <UserOutput name={this.state.name}/>
-        <UserOutput name="Batman"/>
+        <h1>Let's get ReactV</h1>
+        {/* Inefficient way of passing function args */}
+        <button onClick={() => this.switchStateHandler("Chinmay Bhat")}>Switch Name</button>
+        {/* Better way of passing function args */}
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchStateHandler.bind(this, "Chinmay")}
+        >Hobbies: Badminton, Football, Carrom
+        </Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          changed={this.nameChangeHandler}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
+        <Person 
+          name={this.state.persons[3].name} 
+          age={this.state.persons[3].age}/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return(
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I am <b>{props.name}</b> and I am <b>{props.age}</b> years old.</p>
             <p>{props.children}</p>
             <input type="text" value={props.name} onChange={props.changed}/>
